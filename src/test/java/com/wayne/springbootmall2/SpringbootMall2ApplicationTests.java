f246cbd1e8ded5b35d3869bd04b8a603cd5185b4
Wayne <waynelin30078@gmail.com>
2025-03-17 20:41:13 +0800

    initialize Spring Boot project

---
+++ src/test/java/com/wayne/springbootmall2/SpringbootMall2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wayne.springbootmall2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootMall2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
