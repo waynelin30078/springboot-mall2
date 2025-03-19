b1fda6558c01a44141a634d7ca8bed70d913dd7e
Wayne <waynelin30078@gmail.com>
2025-03-19 10:21:00 +0800

    商品功能 - Refactor category，使用Enum 類型

--- src/main/java/com/wayne/springbootmall2/constant/ProductCategory.java
+++ src/main/java/com/wayne/springbootmall2/constant/ProductCategory.java
@@ -1,4 +1,7 @@
 package com.wayne.springbootmall2.constant;
 
 public enum ProductCategory {
+    FOOD,
+    CAR,
+    E_BOOK
 }
--- src/main/java/com/wayne/springbootmall2/model/Product.java
+++ src/main/java/com/wayne/springbootmall2/model/Product.java
@@ -1,12 +1,14 @@
 package com.wayne.springbootmall2.model;
 
+import com.wayne.springbootmall2.constant.ProductCategory;
+
 import java.util.Date;
 
 public class Product {
 
     private Integer productId;
     private String productName;
-    private String category;
+    private ProductCategory category;
     private String imageUrl;
     private Integer price;
     private Integer stock;
@@ -30,12 +32,12 @@ public class Product {
         this.productName = productName;
     }
 
-    public String getCategory() {
+    public ProductCategory getCategory() {
         return category;
     }
 
-    public void setCategory(String category) {
-        this.category = category;
+    public void setCategory(ProductCategory category) {
+        this.category =category;
     }
 
     public String getImageUrl() {
